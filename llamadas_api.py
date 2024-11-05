ef34282d4208b3629c670b6f7315117bb7bfc40a
GermanAbuarab <gabuarab@correo.um.edu.uy>
2024-11-04 23:03:04 -0300

    Third commit

--- llamadas_api.py
+++ llamadas_api.py
@@ -1,5 +1,13 @@
-# Generar un a funcion que llame a la api de libros y devuelva el primer titulo que
-# encuentre dado un supuesto nombre del libro
+# Generar un a funcion que llame a la api de libros y devuelva
+# la cantidad de libros que encontro  y
+# el primer titulo que encuentre
+# dado un supuesto nombre del libro
+# en caso de no enontrar nignuno , devolver que no se encontro ningun libro para ese nombre,
+
+
+
+
+
 
 
 
